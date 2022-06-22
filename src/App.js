a5752676cef2c7b4e6361f62764c4897c7247ea0
BoyThawatchai <thawatkw@ais.co.th>
2022-06-22 13:36:01 +0700

    Edit component

--- src/App.js
+++ src/App.js
@@ -11,24 +11,27 @@ import tattoos from "./data/tattoos";
 function App() {
   const [selectedTattoo, setSelectedTattoo] = useState(null);
 
-  function onTattooOpenClick() {
-    setSelectedTattoo(tattoos[1]);
+  function onTattooOpenClick(theTattoo) {
+    setSelectedTattoo(theTattoo);
+  }
+
+  function onTattooCloseClick() {
+    setSelectedTattoo(null)
   }
 
   const tattooElements = tattoos.map((tattoo, index) => {
-    return <TattooItem key={index} tattoo={tattoo}/>
+    return <TattooItem key={index} tattoo={tattoo} onTattooClick={onTattooOpenClick}/>
   }) 
-
 
   let tattooPost = null;
   if (!!selectedTattoo) {
-    tattooPost = <TattooPost tattoo={selectedTattoo}/>
+    tattooPost = <TattooPost tattoo={selectedTattoo} onBgClick={onTattooCloseClick}/>
   }
   return (
     <>
       <div className="App">
         <AppHeader />
-        <button onClick={onTattooOpenClick}>Click</button>
+        {/* {<button onClick={onTattooOpenClick}>Click</button> } */}
       </div>
       <div className="app-grid">
         {tattooElements}
